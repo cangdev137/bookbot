c2f24a45b5e9f2e73412280a0381521317610c48
dev <carlosanguiano101@gmail.com>
2025-09-02 17:07:22 -0700

    add bash script for easier compilation

---
+++ bookbot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#call the bookbot
+
+if [ "$#" -eq 0 ]; then
+    echo "Error: no text file was provided." >&2
+    echo "Usage: $0 [flags] <path_to_text_file>" >&2
+    exit 1
+fi
+
+verbose=false
+while getopts "v" flag; do
+    case "${flag}" in 
+        v)
+            verbose=true
+            ;;
+
+        *)
+            echo "UNRECOGNIZED FLAG"; >&2
+            echo "Usage: $0 [-v] <path_to_txt_file>" >&2
+            exit 1
+    esac
+done
+
+if [[ $verbose == true ]]; then
+    verbose_arg="-v"
+else
+    verbose_arg=""
+fi
+
+exec python3 main.py "${verbose_arg}" "${@: -1}"
