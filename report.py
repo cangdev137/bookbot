7d39cfe4199b8577736bc66b5e6040980e3f913b
dev <carlosanguiano101@gmail.com>
2025-09-03 16:07:32 -0700

    add longest/shortest word stat

--- report.py
+++ report.py
@@ -3,18 +3,23 @@ from stats import *
 #print report on text data
 def print_report(path, book_text, want_verbose_report=False, want_truncated_counts=False, max_char_counts=1000):
     #get individual words from text
-    individual_words = get_words(book_text)
-    num_words = len(individual_words)
-
-    #get character and line conts
+    book_words = get_words(book_text)
+    #get stats about the words in the book
+    num_words = len(book_words)
+    (average_word_length, shortest_word, longest_word) = get_word_length_stats(book_words)
+    #get stats about the characters in the book
     char_counts = sort_char_counts(get_char_counts(book_text, want_verbose_report))
     total_lines = 0
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
+
     print(" ----------- Word Count ----------")
     print(f"Found {num_words} total words.")
-    print(f"Text averages {get_avg_word_length(individual_words)} chars per word.")
+    print(f"Text averages {average_word_length} chars per word.")
+    print(f"Shortest word is {len(shortest_word)} chars long. ({shortest_word})")
+    print(f"Longest word is {len(longest_word)} chars long. ({longest_word})")
+
 
     print("--------- Character Counts -------")
     print(f"Found {get_total_char_count(char_counts)} total characters.")
